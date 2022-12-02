73815e73bfd5752e0e9690a5e03b96b084eb25ec
Wan Shahruddin <wanshahruddin.bestweb@gmail.com>
2022-12-02 16:28:51 +0800

    Serving html files

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 const http = require("http");
+const fs = require("fs");
+
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        data: 'Hello World!',
-    }));
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        fs.readFile("page/home.html", 'utf8', (err, data) => {
+            if (err) throw err;
+
+            res.write(data);
+
+            res.end();
+        });
+
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        fs.readFile("page/about.html", 'utf8', (err, data) => {
+            if (err) throw err;
+
+            res.write(data);
+
+            res.end();
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+
+        fs.readFile("page/404.html", 'utf8', (err, data) => {
+            if (err) throw err;
+
+            res.write(data);
+
+            res.end();
+        });
+    }
 });
 
 console.log(`server is running at http://localhost:${PORT}`);
