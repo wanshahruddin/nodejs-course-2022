f5d51d520cd91fb10fa1aec1e2e9f90e64a74709
Wan Shahruddin <wanshahruddin.bestweb@gmail.com>
2022-12-02 16:10:26 +0800

    Installed nodemon

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -7,4 +8,6 @@ const server = http.createServer((req, res) => {
     }));
 });
 
-server.listen(3000);
+console.log(`server is running at http://localhost:${PORT}`);
+
+server.listen(PORT);
